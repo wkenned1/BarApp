3732b805675828e8470e1abbbe0460977b99ac7f
William Kennedy <wkenned1@binghamton.edu>
2022-08-03 19:23:21 -0400

    add location model

--- lib/ui/map_test.dart
+++ lib/ui/map_test.dart
@@ -1,6 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import '../models/location_model.dart';
+
+List<LocationModel> getDefaultLocations() {
+  return <LocationModel>[
+    LocationModel(
+        markerId: "Bijou",
+        position: LatLng(42.351238, -71.064209),
+        infoWindowTitle: "Bijou",
+        address: "51 Stuart St, Boston, MA 02116",
+        type: "bar"),
+    LocationModel(
+        markerId: "Fenway Johnie's",
+        position: LatLng(42.346111, -71.099281),
+        infoWindowTitle: "Fenway Johnie's",
+        address: "96 Brookline Ave, Boston, MA 02215",
+        type: "bar"),
+    LocationModel(
+        markerId: "Loretta's Last Call",
+        position: LatLng(42.347328, -71.094490),
+        infoWindowTitle: "Loretta's Last Call",
+        address: "1 Lansdowne St, Boston, MA 02215",
+        type: "bar"),
+    LocationModel(
+        markerId: "Game On!",
+        position: LatLng(42.347031, -71.098389),
+        infoWindowTitle: "Game On!",
+        address: "82 Lansdowne St, Boston, MA 02215",
+        type: "bar"),
+    LocationModel(
+        markerId: "Two Saints Tavern",
+        position: LatLng(41.187030, -73.130230),
+        infoWindowTitle: "Two Saints Tavern",
+        address: "1884 Main St, Stratford, CT 06615",
+        type: "bar"),
+    LocationModel(
+        markerId: "Lansdowne Pub",
+        position: LatLng(42.347359, -71.095078),
+        infoWindowTitle: "Lansdowne Pub",
+        address: "9 Lansdowne St, Boston, MA 02215",
+        type: "bar"),
+  ];
+}
+
 class MapSample extends StatefulWidget {
   @override
   _MapState createState() => _MapState();
@@ -24,16 +67,21 @@ class _MapState extends State<MapSample> {
       "assets/images/bar_icon.png",
     );
 
-    markers.add(Marker(
-      //add start location marker
-      markerId: MarkerId(startLocation.toString()),
-      position: LatLng(42.351238, -71.064209), //position of marker
-      infoWindow: InfoWindow(
-        //popup info
-        title: 'Bijou',
-      ),
-      icon: markerbitmap, //Icon for Marker
-    ));
+    List<LocationModel> locations = getDefaultLocations();
+
+    for (LocationModel location in locations) {
+      markers.add(Marker(
+        //add start location marker
+        markerId: MarkerId(location.markerId),
+        position: location.position, //position of marker
+        infoWindow: InfoWindow(
+          //popup info
+          title: location.infoWindowTitle,
+        ),
+        //TODO: replace harcoded bit marker with function
+        icon: markerbitmap, //Icon for Marker
+      ));
+    }
 
     setState(() {
       //refresh UI
